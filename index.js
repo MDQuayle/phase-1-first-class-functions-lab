be278f11ab6377690e1af9b034695b756d74ccf6
mdquayle <misternopants@gmail.com>
2021-06-10 18:36:23 -0400

    commit tree 382fb9e5df78b3935a2ba72eb88655a6a1c150e5

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(list){
+    const firstTwo = function(list){
+        const boom = [list[0],list[1]]
+        return boom;
+    }
+    return firstTwo(list)
+}
+
+function returnLastTwoDrivers(list){
+    const lastTwo = function(list){
+        const splash = [list[list.length -2],list[list.length -1]]
+        return splash;
+    }
+    return lastTwo(list)
+}
+
+
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(x){
+    function fareMultiplier(y){
+        return y*x
+    }
+    return fareMultiplier
+}
+
+function fareDoubler(y){
+    const doubled = createFareMultiplier(2);
+    return doubled(y);
+}
+
+
+function fareTripler(y){
+    const tripled = createFareMultiplier(3);
+    return tripled(y);
+}
+
+
+function selectDifferentDrivers(one,two){
+    return two(one)
+
+}
